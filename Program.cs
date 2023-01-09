e83e82bd667d6840f80e7cd8c4d40729bcb29782
Jennifer Massi <jennifer.massi@mscoding.org>
2023-01-09 10:57:37 -0800

    Changing strings to ints

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Changing_strings_to_integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int num = 0;
+            string num2 = "50";
+            num2 = Console.ReadLine();
+            Console.WriteLine("Write a number");
+            
+            num = int.Parse(num2);
+
+            Console.WriteLine(num + 40);
+        }
+    }
+}
